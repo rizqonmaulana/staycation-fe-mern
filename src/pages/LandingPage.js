9dfc99acfea87d8d11dec9a4fb2f5b6e728303ad
Rizqon Maulana <rizqonmaulana5@gmail.com>
2021-08-07 22:47:30 +0700

    slicing categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,13 +3,14 @@ import React, { Component } from "react";
 import Header from "../parts/Header";
 import Hero from "../parts/Hero";
 import MostPicked from "../parts/MostPicked";
+import Categories from "../parts/Categories";
 
 import landingPage from "../json/landingPage.json";
 
 export default class LandingPage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.refMostPicked = React.createRef()
+    this.refMostPicked = React.createRef();
   }
   render() {
     console.log(this.props);
@@ -17,7 +18,11 @@ export default class LandingPage extends Component {
       <>
         <Header {...this.props}></Header>
         <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
-        <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
+        <Categories data={landingPage.categories} />
       </>
     );
   }
