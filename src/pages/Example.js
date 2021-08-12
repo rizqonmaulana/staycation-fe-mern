07ae8c9565093f19548c24923b8565bfca22d0a2
Rizqon Maulana <rizqonmaulana5@gmail.com>
2021-08-12 15:06:25 +0700

    slicing input date component

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-
-import InputNumber from "../elements/Form/InputNumber";
+import { InputDate } from "../elements/Form";
 
 export default class Example extends Component {
   state = {
-    value: "1",
+    value: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
   };
 
   handleChange = (e) => {
@@ -19,14 +22,12 @@ export default class Example extends Component {
           style={{ height: "100vh" }}
         >
           <div className="col-auto">
-            <InputNumber
+            <InputDate
               max={30}
-              isSuffixPlural
-              suffix=" night"
               onChange={this.handleChange}
               name="value"
               value={this.state.value}
-            ></InputNumber>
+            />
           </div>
         </div>
       </div>
